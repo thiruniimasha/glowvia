5df0b1e97d85e95c0adf516da9dbe1e23a2697c4
thiruniimasha <t.hiruniimasha2002@gmail.com>
2025-06-28 21:38:27 +0530

    setup client and server project

---
+++ client/src/assets/assets.js
@@ -0,0 +1,118 @@
+import logo from "./logo.png";
+import search_icon from "./search_icon.svg";
+import remove_icon from "./remove_icon.svg";
+import arrow_right_icon_colored from "./arrow_right_icon_colored.svg";
+import star_icon from "./star_icon.svg";
+import star_dull_icon from "./star_dull_icon.svg";
+import cart_icon from "./cart_icon.svg";
+import nav_cart_icon from "./nav_cart_icon.svg";
+import add_icon from "./add_icon.svg";
+import refresh_icon from "./refresh_icon.svg";
+import product_list_icon from "./product_list_icon.svg";
+import order_icon from "./order_icon.svg";
+import profile_icon from "./profile_icon.png";
+import menu_icon from "./menu_icon.svg";
+import delivery_truck_icon from "./delivery_truck_icon.svg";
+import coin_icon from "./coin_icon.svg";
+import box_icon from "./box_icon.svg";
+import trust_icon from "./trust_icon.svg";
+import black_arrow_icon from "./black_arrow_icon.svg";
+import white_arrow_icon from "./white_arrow_icon.svg";
+import add_address_iamge from "./add_address_image.svg";
+import main_banner_bg from "./main_banner_bg.jpeg";
+import main_banner_bg_sm from "./main_banner_bg_sm.jpeg";
+import cleanser_image from "./cleanser_image.png";
+import toner_mist_image from "./toner_mist_image.jpg";
+import serum_image from "./serum_image.jpg";
+import moisturizer_image from "./moisturizer_image.jpg";
+import sunscreen_image from "./sunscreen_image.jpg";
+import mask_image from "./mask_image.jpg";
+import lip_eye_image from "./lip_eye_image.jpg";
+import gift_set_image from "./gift_set_image.jpg";
+
+
+export const assets = {
+  logo,
+  search_icon,
+  remove_icon,
+  arrow_right_icon_colored,
+  star_icon,
+  star_dull_icon,
+  cart_icon,
+  nav_cart_icon,
+  add_icon,
+  refresh_icon,
+  product_list_icon,
+  order_icon,
+  profile_icon,
+  menu_icon,
+  delivery_truck_icon,
+  coin_icon,
+  trust_icon,
+  black_arrow_icon,
+  white_arrow_icon,
+  add_address_iamge,
+  box_icon,
+  main_banner_bg,
+  main_banner_bg_sm,
+  cleanser_image,
+  toner_mist_image,
+  serum_image,
+  moisturizer_image,
+  sunscreen_image,
+  mask_image,
+  lip_eye_image,
+  gift_set_image,
+};
+
+export const categories = [
+  {
+    text: "Cleansers",
+    path: "cleansers",
+    image: cleanser_image, // gentle foaming or gel-based product image
+    bgColor: "#FCEBEC", // soft blush pink
+  },
+  {
+    text: "Toners & Mists",
+    path: "toners-mists",
+    image: toner_mist_image,
+    bgColor: "#EAF5F2", // light minty green
+  },
+  {
+    text: "Serums",
+    path: "serums",
+    image: serum_image,
+    bgColor: "#FFF2E9", // peach-beige glow
+  },
+  {
+    text: "Moisturizers",
+    path: "moisturizers",
+    image: moisturizer_image,
+    bgColor: "#EAE6F4", // lavender tint
+  },
+  {
+    text: "Sunscreens",
+    path: "sunscreens",
+    image: sunscreen_image,
+    bgColor: "#FDF6E9", // creamy ivory
+  },
+  {
+    text: "Masks & Exfoliators",
+    path: "masks-exfoliators",
+    image: mask_image,
+    bgColor: "#F6E7EA", // soft rose
+  },
+  {
+    text: "Lip & Eye Care",
+    path: "lip-eye-care",
+    image: lip_eye_image,
+    bgColor: "#FBEED7", // subtle nude
+  },
+  {
+    text: "Gift Sets",
+    path: "gift-sets",
+    image: gift_set_image,
+    bgColor: "#F0F8FF", // light pastel blue
+  },
+];
+
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainBanner from '../components/MainBanner'
+import Categories from '../components/Categories'
+
+function Home() {
+  return (
+    <div className='mt-10'>
+        <MainBanner />
+        <Categories />
+      
+    </div>
+  )
+}
+
+export default Home
