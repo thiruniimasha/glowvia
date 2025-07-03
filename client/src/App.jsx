27b0c3b1d41e3ac1a62deb796c77f0277f205611
thiruniimasha <t.hiruniimasha2002@gmail.com>
2025-07-03 16:24:10 +0530

    create all product page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import { Toaster } from 'react-hot-toast'
 import Footer from './components/Footer'
 import { useAppContext } from './context/AppContext'
 import Login from './components/Login'
+import AllProduct from './pages/AllProduct'
 
 function App() {
 
@@ -24,6 +25,7 @@ function App() {
       <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path='/' element={<Home />} />
+           <Route path='/products' element={<AllProduct />} />
         </Routes>
       </div>
      {!isSellerPath && <Footer/>}
