e146f9a92c0d3d5ede9cc706c45c93948a57d910
thiruniimasha <t.hiruniimasha2002@gmail.com>
2025-08-07 22:14:46 +0530

    create product list page

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -6,9 +6,9 @@ const SellerLayout = () => {
 
     const { setIsSeller } = useAppContext();
 
-   
 
-  
+
+
 
     const sidebarLinks = [
         { name: "Add Product", path: "/seller", icon: assets.add_icon },
@@ -16,7 +16,7 @@ const SellerLayout = () => {
         { name: "Orders", path: "/seller/orders", icon: assets.order_icon },
     ];
 
-    const logout = async () =>{
+    const logout = async () => {
         setIsSeller(false);
     }
 
@@ -24,7 +24,7 @@ const SellerLayout = () => {
         <>
             <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white">
                 <Link to='/'>
-                    <img src={assets.logo} alt="logo" className=" cursor- pointer h-15"/>
+                    <img src={assets.logo} alt="logo" className=" cursor- pointer h-15" />
                 </Link>
                 <div className="flex items-center gap-5 text-gray-500">
                     <p>Hi! Admin</p>
@@ -32,25 +32,25 @@ const SellerLayout = () => {
                 </div>
             </div>
             <div className="flex">
-                <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
-                {sidebarLinks.map((item) => (
-                    <NavLink to={item.path} key={item.name} end={item.path === "/seller"}
-                        className={({isActive}) => `flex items-center py-3 px-4 gap-3 
+                <div className="md:w-64 w-16 border-r h-[95vh] text-base border-gray-300 pt-4 flex flex-col ">
+                    {sidebarLinks.map((item) => (
+                        <NavLink to={item.path} key={item.name} end={item.path === "/seller"}
+                            className={({ isActive }) => `flex items-center py-3 px-4 gap-3 
                             ${isActive ? "border-r-4 md:border-r-[6px] bg-primary/10 border-primary text-primary"
-                                : "hover:bg-gray-100/90 border-white"
-                            }`
-                        }
-                    >
-                        <img src={item.icon} alt="" className="w-7 h-7"/>
-                        <p className="md:block hidden text-center">{item.name}</p>
-                    </NavLink>
-                ))}
+                                    : "hover:bg-gray-100/90 border-white"
+                                }`
+                            }
+                        >
+                            <img src={item.icon} alt="" className="w-7 h-7" />
+                            <p className="md:block hidden text-center">{item.name}</p>
+                        </NavLink>
+                    ))}
+                </div>
+
+                <Outlet />
+
             </div>
 
-            <Outlet/>
-
-            </div>
-            
         </>
     );
 };
