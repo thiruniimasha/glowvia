0d84d695bba1a9782c954dbe2540b7ac72d9a1f4
thiruniimasha <t.hiruniimasha2002@gmail.com>
2025-08-08 14:06:02 +0530

    setup backend

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+//allow multiple origins
+const allowedOrigins = ['http://localhost:5173']
+
+//middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({origin: allowedOrigins, credentials: true}));
+
+app.get('/', (req, res) => res.send("API is working"));
+
+app.listen(port, ()=>{
+    console.log(`Server is running on http://localhost:${port}`)
+})
