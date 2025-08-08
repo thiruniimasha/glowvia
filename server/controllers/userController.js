ce7b2c92a87df1cf235eb760148cc37d9d09b274
thiruniimasha <t.hiruniimasha2002@gmail.com>
2025-08-08 17:47:58 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import { use } from "react";
 
 //Register User : /api/user/register
 export const register = async (req, res) => {
@@ -13,7 +14,7 @@ export const register = async (req, res) => {
         const existingUser = await User.findOne({ email })
 
         if (existingUser)
-            return res.json({ success: false, message: 'Missing Details' })
+            return res.json({ success: false, message: 'User already exists.' })
 
         const hashedPassword = await bcrypt.hash(password, 10)
 
@@ -28,13 +29,49 @@ export const register = async (req, res) => {
             maxAge: 7 * 24 * 60 * 60 * 1000,   // Cookie expiration time
         })
 
-        return res.json({success: true, user: {email: user.email, name: user.name}})
+        return res.json({ success: true, user: { email: user.email, name: user.name } })
 
-    } catch (error){
+    } catch (error) {
         console.log(error.message);
-        res.json({ success:false, message: error.message});
+        res.json({ success: false, message: error.message });
     }
 
 
 
+}
+
+//Login user : /api/user/login
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password)
+            return res.json({ success: false, message: 'Email and password are required' });
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.json({ success: false, message: 'Invalid email or password' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch)
+            return res.json({ success: false, message: 'Invalid email or password' });
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+        res.cookie('token', token, {
+            httpOnly: true,    //prevent javascript to access cookie
+            secure: process.env.NODE_ENV === 'production',  //Use secure cookies in production
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',    //CSRF protection
+            maxAge: 7 * 24 * 60 * 60 * 1000,   // Cookie expiration time
+        })
+
+        return res.json({ success: true, user: { email: user.email, name: user.name } })
+
+    }catch(error){
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
 }
