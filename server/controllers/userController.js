0b0c7ea6f93ed02b484116fbfa4716a7a145626a
thiruniimasha <t.hiruniimasha2002@gmail.com>
2025-08-08 18:03:59 +0530

    login function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -37,4 +37,43 @@ export const register = async (req, res) => {
 
 
 
+<<<<<<< Updated upstream
+=======
+}
+
+//Login user : /api/user/login
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password)
+            return res.json({ success: false, message: 'Email and password are required' });
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.json({ success: false, message: 'Invalid email or password' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch)
+            return res.json({ success: false, message: 'Invalid password' });
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+        res.cookie('token', token, {
+            httpOnly: true,    //prevent javascript to access cookie
+            secure: process.env.NODE_ENV === 'production',  //Use secure cookies in production
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',    //CSRF protection
+            maxAge: 7 * 24 * 60 * 60 * 1000,   // Cookie expiration time
+        })
+
+        return res.json({ success: true, user: { email: user.email, name: user.name } })
+
+    }catch(error){
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+>>>>>>> Stashed changes
 }
