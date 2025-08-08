46b595aefd4d14c05e22827474e875566394bd2a
thiruniimasha <t.hiruniimasha2002@gmail.com>
2025-08-08 14:48:40 +0530

    setup database

--- server/configs/db.js
+++ server/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async() => {
     try{
         mongoose.connection.on('connected', ()=> console.log("Database Connected"));
-        await mongoose.connect(`${Process.env.MONGODB_URI}/glowia`)
+        await mongoose.connect(`${process.env.MONGODB_URI}/glowia`)
     } catch(error){
         console.error(error.message);
     }
