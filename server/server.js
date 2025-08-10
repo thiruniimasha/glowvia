2437a67f2791371bd4b463d9383379acfa9abe72
thiruniimasha <t.hiruniimasha2002@gmail.com>
2025-08-10 15:56:32 +0530

    seller features

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import connectDB from './configs/db.js';
 import 'dotenv/config';
 import userRouter from './routes/userRoute.js';
+import sellerRouter from './routes/sellerRoute.js';
 
 
 const app = express();
@@ -21,6 +22,8 @@ app.use(cors({origin: allowedOrigins, credentials: true}));
 
 app.get('/', (req, res) => res.send("API is working"));
 app.use('/api/user', userRouter)
+app.use('/api/seller', sellerRouter)
+
 
 app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`)
