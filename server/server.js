c805458d1bcb4d4d65292f10c005e0fec69101cf
thiruniimasha <t.hiruniimasha2002@gmail.com>
2025-08-12 13:14:32 +0530

    user address

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import sellerRouter from './routes/sellerRoute.js';
 import connectCloudinary from './configs/cloudinary.js';
 import productRouter from './routes/productRoute.js';
 import cartRouter from './routes/cartRoute.js';
+import addressRouter from './routes/addressRoute.js';
 
 
 const app = express();
@@ -29,6 +30,7 @@ app.use('/api/user', userRouter)
 app.use('/api/seller', sellerRouter)
 app.use('/api/product', productRouter)
 app.use('/api/cart', cartRouter)
+app.use('/api/address', addressRouter)
 
 
 
